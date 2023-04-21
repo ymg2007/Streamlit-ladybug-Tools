f6c33ee5b1f149771a2257060b1e6322d5ece2cb
minggangyin <ymg2007@qq.com>
2023-04-21 16:32:57 +0800

    增加默认选项，下载安徽的气象数据

--- windrose_monthly.py
+++ windrose_monthly.py
@@ -6,6 +6,33 @@ import streamlit as st
 from ladybug.epw import EPW
 import plotly.io as pio
 
+import requests
+import zipfile
+import os
+
+url = "https://climate.onebuilding.org/WMO_Region_2_Asia/CHN_China/AH_Anhui/CHN_AH_Anqing.584240_CSWD.zip"
+response = requests.get(url)
+
+# Save the downloaded file
+zip_file_path = "CHN_AH_Anqing.584240_CSWD.zip"
+with open(zip_file_path, "wb") as file:
+    file.write(response.content)
+
+# Unzip the file
+with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+    zip_ref.extractall("")
+
+# Find the EPW file in the extracted directory
+epw_file = None
+for file in os.listdir():
+    if file.endswith(".epw"):
+        epw_file = file
+        break
+
+if epw_file:
+    print(f"EPW file found: {epw_file}")
+else:
+    print("No EPW file found.")
 
 uploaded_file = st.file_uploader("Upload EPW file", type="epw")
 if uploaded_file is not None:
@@ -15,7 +42,7 @@ if uploaded_file is not None:
     st.success("File uploaded successfully!")
     epw = EPW("new.epw")
 else:
-    epw= './CHN_SH_Shanghai.epw'
+    epw= epw_file
 
 
 # Python
